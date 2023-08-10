bcdbdf98738a823d739ca101310b966c2017306b
Nelo-T. Wallus <nelo@wallus.de>
2023-08-10 02:24:21 +0200

    Add String MustString

---
+++ value_string.go
@@ -0,0 +1,33 @@
+package dataparse
+
+import (
+	"fmt"
+	"strings"
+)
+
+func (v Value) String() (string, error) {
+	if v.Data == nil {
+		return "", ErrValueIsNil
+	}
+	return fmt.Sprintf("%v", v.Data), nil
+}
+
+func (v Value) ListString(sep string) ([]string, error) {
+	val, err := v.String()
+	if err != nil {
+		return nil, err
+	}
+
+	return strings.Split(val, sep), nil
+}
+
+func (v Value) MustString() string {
+	return fmt.Sprintf("%v", v.Data)
+}
+
+func (v Value) MustListString(sep string) []string {
+	if val, err := v.ListString(sep); err == nil {
+		return val
+	}
+	return []string{}
+}
