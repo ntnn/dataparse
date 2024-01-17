712fe6479637e3bec3489d09c0db97d9f5adc227
Nelo-T. Wallus <nelo@wallus.de>
2024-01-17 15:36:22 +0100

    Support parsing different strings as false/true

--- valueBool.go
+++ valueBool.go
@@ -2,9 +2,40 @@ package dataparse
 
 import (
 	"fmt"
+	"slices"
 	"strconv"
+	"strings"
 )
 
+// BoolStringsFalse are strings that will be interpreted as false by
+// Value.Bool.
+var BoolStringsFalse = []string{
+	"",
+
+	"0",
+	"no",
+	"n",
+	"false",
+
+	"na",
+	"n/a",
+}
+
+// BoolStringsTrue are strings taht will be interpreted as true by
+// Value.Bool.
+var BoolStringsTrue = []string{
+	"1",
+	"yes",
+	"y",
+	"true",
+}
+
+// Bool returns a boolean for the underlying lower cased value.
+//
+// Strings in BoolStringsFalse and BoolStringsTrue are considered to be
+// false and true respectively.
+//
+// If neither applies strconv.ParseBool is utilized.
 func (v Value) Bool() (bool, error) {
 	if v.Data == nil {
 		return false, ErrValueIsNil
@@ -18,8 +49,14 @@ func (v Value) Bool() (bool, error) {
 		s = fmt.Sprintf("%v", v.Data)
 	}
 
-	if s == "" {
+	s = strings.ToLower(s)
+
+	if slices.Contains(BoolStringsFalse, s) {
 		return false, nil
+	}
+
+	if slices.Contains(BoolStringsTrue, s) {
+		return true, nil
 	}
 
 	b, err := strconv.ParseBool(s)
